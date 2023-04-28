1695897eacbed464203b32e66d17adf6c20be297
Muhammad Hassan <hassanmuhammad221@gmail.com>
2023-04-28 17:50:08 +0500

    Add index template

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
+	tpl := template.Must(template.ParseFiles("index.html"))
+
 	srv := http.DefaultServeMux
 	srv.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Got request")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello, world!"))
+
+		tpl.ExecuteTemplate(w, "index.html", struct {
+			Title   string
+			BGColor string
+		}{
+			Title:   "GKE-Demo",
+			BGColor: "red",
+		})
 	})
 
 	log.Println("Listening on :8888")
